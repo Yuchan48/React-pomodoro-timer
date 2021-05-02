6bdb2dc997a066cfa9a8787210761b9e2030e1d8
Yuchan Iizuka <yuchan4869@gmx.com>
2021-05-02 16:23:40 +0200

    init

--- calendar/script.js
+++ calendar/script.js
@@ -43,6 +43,7 @@ const renderCalendar = () => {
   ];
 
   document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+  
   document.querySelector(".date p").innerHTML = new Date().toDateString();
 
   let days = "";
